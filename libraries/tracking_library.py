12910d3e0c560bceae6ea06db07ad1a6e939e8e6
Lory <lory.calcaterra@gmail.com>
2019-09-03 17:55:21 +0100

    crop video around rat centroid

--- libraries/tracking_library.py
+++ libraries/tracking_library.py
@@ -170,5 +170,95 @@ def track_rat(video, background):
 
     return 1
 
+# Track rat through entire video
+def crop_rat(video, background, window_size, output_name):
+
+    # Create output filenames
+    csv_path = output_name + '.csv'
+    avi_path = output_name + '.avi'
+    
+    # Open output video
+    
+    
+    # Compute crop size (half of window size)
+    crop_size = np.int(window_size / 2)
+       
+    
+    # Read current frame
+    success, image = video.read()
+
+    # Measure dimensions
+    width = image.shape[1]
+    height = image.shape[0]
+    num_frames = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+
+    # Create "larger" frame to contain video frame with border
+     container = np.zeros((height + 2 * crop_size, width + 2 * crop_size), dtype=np.uint8)
+
+    # Reset video to first frame
+    video.set(cv2.CAP_PROP_POS_FRAMES, 0)
+
+    # Read and process each frame
+    cv2.namedWindow("Display")
+    for frame in range(0, num_frames):
+
+        # Read current frame
+        success, image = video.read()
+
+        # Convert to grayscale
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+        # Subtract background (darker values positive)
+        subtracted = cv2.subtract(background, gray)
+
+        # Threshold
+        level, threshed = cv2.threshold(subtracted, 5, 255, cv2.THRESH_BINARY)
+
+        # Open (Erode then Dialate) to remove tail and noise
+        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(15,15))
+        opened = cv2.morphologyEx(threshed, cv2.MORPH_OPEN, kernel)
+        
+        
+        
+        # Find Binary Contours            
+        ret, contours, hierarchy = cv2.findContours(np.copy(opened),cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)        
+        if len(contours) == 0:
+            # Store a NaN in the tracking thing that doesn't exist yet
+            continue
+
+        # Get largest particle
+        largest_cnt, area = get_largest_contour(contours)
+
+
+        # Get centroid (and moments)          
+        M = cv2.moments(largest_cnt)
+        cx = (M['m10']/M['m00'])
+        cy = (M['m01']/M['m00'])
+        
+        # Insert grayscale frame into the container
+        container[crop_size:(height+crop_size), crop_size:(width+crop_size)] = gray
+        
+        
+        # Offset centroid position to container coordinates
+        container_cx = np.int(cx + crop_size)
+        container_cy = np.int(cy + crop_size)
+        
+        # Crop around the centroid position in the container image
+        crop = container[(container_cy - crop_size):(container_cy + crop_size), (container_cx - crop_size):(container_cx + crop_size)]
+        
+
+        # Write output video frame
+        
+                      
+        # Display
+        display = np.hstack((diff, crop))
+     
+        #cv2.waitKey(1)
+
+    # Cleanup
+    cv2.destroyAllWindows()
+    # Clouse output video
+
+    return 1
 
 #FIN
---
+++ scripts/behaviour/Crop_rat_in_video.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Track_rat_in_video.py
+
+Finds the nose and tail coordinates for every frame of a behaviour video
+
+@author: You
+"""
+import os
+#os.sys.path.append('/home/kampff/Repos/Pac-Rat/libraries')
+os.sys.path.append('D:/Repos/Pac-Rat/libraries')
+import glob
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
+import tracking_library as tracking
+
+# Reload modules
+import importlib
+importlib.reload(tracking)
+
+# Specify video file name
+#video_path = '/home/kampff/LC/videos/pre.avi'
+video_path = 'C:/Users/KAMPFF-LAB-ANALYSIS3/Videos/tracking_video.mp4'
+# Open video
+video = cv2.VideoCapture(video_path)
+
+# Compute background (for first 10 minutes of a session, using approx. 50 frames)
+background = tracking.compute_background_median(video, 0, 72000, 1440)
+
+# Test tracking
+importlib.reload(tracking)
+test = tracking.crop_rat(video, background, 640)
+
+# Display
+#plt.imshow(background, cmap='gray')
+#plt.show()
+
+#FIN
