57aff0083aa5a26124a7e7cb50905b2bc4344bae
Lory <lory.calcaterra@gmail.com>
2019-09-26 14:54:09 +0100

    session parser
    from .csv summary or each rat to paths to different levels

---
+++ libraries/parser_library.py
@@ -0,0 +1,131 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 17 14:56:17 2019
+
+parser libray.py
+
+functions to parse files 
+
+@author: KAMPFF-LAB-ANALYSIS3
+
+"""
+import os
+#os.sys.path.append('/home/kampff/Repos/Pac-Rat/libraries')
+os.sys.path.append('D:/Repos/Pac-Rat/libraries')
+import numpy as np
+
+
+
+
+# hard drive base path where the sessions are stored - My Book Duo (F:)
+
+#hardrive_path = r'F:/'
+
+#rat_summary_table_path =  hardrive_path + 'Videogame_Assay/AK_48.4_IrO2.csv'
+
+#each rat has a summary .csv file where the following infos are stored: 
+
+#Path, Session, Level, Surgery, Name, Weight, Include, Comments
+
+# Path[0] = path to the session which starts with the following format :'Videogame_Assay/AK_rat_name/date-time' (Example : 'Videogame_Assay/AK_33.2/2018_05_17-15_34')
+# Session[1] = the date of the session without the time
+# Level[2] = level of the Videogame, Level 0,1,2,3...
+# Surgery[3] = indicate if t he session has been done prior or after surgery, pre = before surgery, post = after surgery
+# Name[4] = name of the Level (habituation, training...)
+# Weight[5] = weight of the rat in grams 
+# Include[6] = inclusion criteria Y or N
+# Comments[7] = comments regarding the session 
+
+#the data starts from row number 2 ( row 0 = rat name and row 1 = title of the column)
+#each fx return a list containing the str object = all the session paths for a Level 
+# list items format example = Videogame_Assay/AK_48.4/2019_06_24-17_10
+
+
+def Level_1_paths(rat_summary_table_path):
+    rat_summary = np.genfromtxt(rat_summary_table_path, delimiter = ',', skip_header = 2 , dtype = str)
+    Level_1 = []
+    for row in range(len(rat_summary)):
+        if rat_summary[row][2] == 'Level 1' and rat_summary[row][6] == 'Y':
+            Level_1.append(rat_summary[row][0])
+        else:
+            continue
+    return Level_1
+    
+
+def Level_2_pre_paths(rat_summary_table_path):
+    rat_summary = np.genfromtxt(rat_summary_table_path, delimiter = ',', skip_header = 2 , dtype = str)
+    Level_2_pre = []  
+    for row in range(len(rat_summary)):
+        if not rat_summary[row][6] == 'N' and rat_summary[row][2] == 'Level 2' and rat_summary[row][3]== 'pre':
+            Level_2_pre.append(rat_summary[row][0])
+        else:
+            continue
+    return Level_2_pre
+    
+
+def Level_2_post_paths(rat_summary_table_path):
+    rat_summary = np.genfromtxt(rat_summary_table_path, delimiter = ',', skip_header = 2 , dtype = str)
+    Level_2_post = []
+    for row in range(len(rat_summary)):
+        if not rat_summary[row][6] == 'N' and rat_summary[row][2] == 'Level 2' and rat_summary[row][3]== 'post':
+           Level_2_post.append(rat_summary[row][0])
+        else:
+            continue
+    return Level_2_post
+
+
+
+def Level_3_pre_paths(rat_summary_table_path):
+    rat_summary = np.genfromtxt(rat_summary_table_path, delimiter = ',', skip_header = 2 , dtype = str)
+    Level_3_pre = []
+    for row in range(len(rat_summary)):
+        if not rat_summary[row][6] == 'N' and rat_summary[row][2] == 'Level 3' and rat_summary[row][3]== 'pre':
+           Level_3_pre.append(rat_summary[row][0])
+        else:
+            continue
+    return Level_3_pre
+
+
+
+
+def Level_3_post_paths(rat_summary_table_path):
+    rat_summary = np.genfromtxt(rat_summary_table_path, delimiter = ',', skip_header = 2 , dtype = str)
+    Level_3_post = []
+    for row in range(len(rat_summary)):
+        if not rat_summary[row][6] == 'N' and rat_summary[row][2] == 'Level 3' and rat_summary[row][3]== 'post':
+           Level_3_post.append(rat_summary[row][0])
+        else:
+            continue
+    return Level_3_post
+
+
+
+def Level_3_moving_light_paths(rat_summary_table_path):
+    rat_summary = np.genfromtxt(rat_summary_table_path, delimiter = ',', skip_header = 2 , dtype = str)
+    Level_3_moving_light = []
+    for row in range(len(rat_summary)):
+        if not rat_summary[row][6] == 'N' and rat_summary[row][4] == 'moving light':
+           Level_3_moving_light.append(rat_summary[row][0])
+        else:
+            continue
+    return Level_3_moving_light
+
+
+
+
+def Level_3_joystick_paths(rat_summary_table_path):
+    rat_summary = np.genfromtxt(rat_summary_table_path, delimiter = ',', skip_header = 2 , dtype = str)
+    Level_3_joystick = []
+    for row in range(len(rat_summary)):
+        if not rat_summary[row][6] == 'N' and rat_summary[row][4] == 'joystick':
+           Level_3_joystick.append(rat_summary[row][0])
+        else:
+            continue
+    return Level_3_joystick
+
+
+
+
+     
+
+    
---
+++ scripts/behaviour/Sessions_parser.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 23 23:18:16 2019
+
+
+Sessions_parser.py 
+
+it parses a summary .csv file and returns a list of paths for a specific
+videogame level of interest
+
+
+@author: KAMPFF-LAB-ANALYSIS3
+"""
+
+import os
+#os.sys.path.append('/home/kampff/Repos/Pac-Rat/libraries')
+os.sys.path.append('D:/Repos/Pac-Rat/libraries')
+import parser_library as prs
+
+
+
+# hard drive base path where the sessions are stored - My Book Duo (F:)
+
+hardrive_path = r'F:/'
+
+rat_summary_table_path =  hardrive_path + 'Videogame_Assay/AK_48.4_IrO2.csv'
+
+#reminder of .csv summary structure
+
+#each rat has a summary .csv file where the following infos are stored: 
+
+#Path, Session, Level, Surgery, Name, Weight, Include, Comments
+
+# Path[0] = path to the session which starts with the following format :'Videogame_Assay/AK_rat_name/date-time' (Example : 'Videogame_Assay/AK_33.2/2018_05_17-15_34')
+# Session[1] = the date of the session without the time
+# Level[2] = level of the Videogame, Level 0,1,2,3...
+# Surgery[3] = indicate if t he session has been done prior or after surgery, pre = before surgery, post = after surgery
+# Name[4] = name of the Level (habituation, training...)
+# Weight[5] = weight of the rat in grams 
+# Include[6] = inclusion criteria Y or N
+# Comments[7] = comments regarding the session 
+
+#the data starts from row number 2 ( row 0 = rat name and row 1 = title of the column)
+
+
+# Reload modules
+import importlib
+importlib.reload(prs)
+
+
+
+#PARSING THE MAIN EXCEL SHEET WITH RAT SUMMARY INTO LEVELS
+     
+
+Level_1 = prs.Level_1_paths(rat_summary_table_path)
+Level_2_pre = prs.Level_2_pre_paths(rat_summary_table_path)
+Level_2_post = prs.Level_2_post_paths(rat_summary_table_path)
+Level_3_pre = prs.Level_3_pre_paths(rat_summary_table_path)
+Level_3_post = prs.Level_3_post_paths(rat_summary_table_path)
+Level_3_moving_light = prs.Level_3_moving_light_paths(rat_summary_table_path)
+Level_3_joystick = prs.Level_3_joystick_paths(rat_summary_table_path)
+
+
+
+
+
+
+
+
+
